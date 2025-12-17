9fcda6d57842db5636db0b8ce9fd016115e65dfc
bazzal831-png <bazzal831@gmail.com>
2025-12-17 17:52:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smoking-tracker-v15";
+const CACHE_NAME = "smoking-tracker-v16";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
@@ -61,4 +61,5 @@ self.addEventListener("fetch", (event) => {
       });
     })
   );
+
 });
